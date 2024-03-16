bd7041a4edac23adaa0a12f891341b1b4933ced0
Nicole Apono <napononicole@gmail.com>
2024-03-16 17:50:55 +0300

    script.js and style.css

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-// Add event listener to "Nicole Ann" h1 element
-const nicoleAnn = document.querySelector("h1");
 
-nicoleAnn.addEventListener("click", function() {
-    // Toggle visibility of the h2 element
+function toggleContent() {
     const h2 = document.querySelector("h2");
     if (h2.style.display === "none" || h2.style.display === "") {
         h2.style.display = "block";
     } else {
         h2.style.display = "none";
     }
-});
+}
