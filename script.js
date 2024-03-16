dd2263af802a8a048e3f7aa872bfb7c0ebc1ee80
Nicole Apono <napononicole@gmail.com>
2024-03-16 18:06:36 +0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-// Function to toggle content visibility
+// Add event listener to "Nicole Ann" h1 element
+const nicoleAnn = document.querySelector("h1");
+
+// Toggle function to show/hide content
 function toggleContent() {
     const h2 = document.querySelector(".content h2");
     h2.style.display = h2.style.display === "none" ? "block" : "none";
 }
 
-// Add event listener to "Nicole Ann" h1 element
-const nicoleAnn = document.querySelector("h1");
 nicoleAnn.addEventListener("click", toggleContent);
+
