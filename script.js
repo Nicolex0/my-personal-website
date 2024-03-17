0c7b439b35f9813e7b5e51e316ba495ecfc107c9
Nicole Apono <napononicole@gmail.com>
2024-03-17 17:16:32 +0300

    index.html and style.css and script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
 // Function to toggle content visibility
 function toggleContent() {
-    const h2 = document.querySelector("#content h2");
+    const h2 = document.querySelector(".content h2");
     if (h2.style.display === "none" || h2.style.display === "") {
         h2.style.display = "block";
     } else {
